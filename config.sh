afa11d5595b18a906ddec9655244f0c6d5624a8c
jjangga0214 <jjangga@kookmin.ac.kr>
2018-11-28 22:58:59 +0900

    add: gnome-tweak-tool, vim, git-lfs

--- config.sh
+++ config.sh
@@ -1,2 +1,17 @@
+#
+echo "installing gnome-tweak-tool, vim .." 
+sudo apt install gnome-tweak-tool
+sudo apt-get install vim
+# theme and icons
+## preemptive defense
+sudo mkdir ~/.local/share/icons
+sudo mkdir ~/.local/share/themes
+echo "installing icons .."
 sudo cp -R ./icons/* ~/.local/share/icons
+echo "installing themes .."
 sudo cp -R ./themes/* ~/.local/share/themes
+sudo apt-get install gnome-shell-extension-top-icons-plus
+# git lfs
+echo "installing git-lfs..".
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
