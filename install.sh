eceae26b7a33a120c7a5dc2244e7ad4bfb0a46bc
jjangga <jjangga@kookmin.ac.kr>
2018-12-21 23:04:32 +0900

    add: trash-cli

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "installing gnome-tweak-tool, vim, git, gpa, seahorse, xz-utils, snapd(for old ubuntu), unzip, nodejs .." 
-apt install -y gnome-tweak-tool vim git gpa xz-utils seahorse snapd unzip nodejs npm build-essential curl 
+echo "installing gnome-tweak-tool, vim, git, gpa, seahorse, xz-utils, snapd(for old ubuntu), unzip, nodejs, trash-cli .." 
+apt install -y gnome-tweak-tool vim git gpa xz-utils seahorse snapd unzip nodejs npm build-essential  curl trash-cli
 # sudo snap install go --classic
 echo "installing uim for Korean supoort rather than iBus, which is default on ubuntu 18.04"
 apt install uim uim 
