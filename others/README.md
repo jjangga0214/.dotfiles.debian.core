4bbc950d7c216622b46289d780edac1141130f46
GIL B. Chan <bnbcmindnpass@gmail.com>
2019-03-25 13:52:27 +0900

    mkdirs for `--user-data-dir` option.

--- others/README.md
+++ others/README.md
@@ -6,4 +6,4 @@ This installs snap applications.
 
 ## [config.sh](./config.sh)
 
-This currently configures only git. Other config for system or tools may be put here later.
+This configures git. This mkdirs for `--user-data-dir` option.
--- others/config.sh
+++ others/config.sh
@@ -4,3 +4,8 @@ git config --global core.editor 'vi'
 
 git config --global user.name 'GIL B. Chan'
 git config --global user.email 'bnbcmindnpass@gmail.com'
+
+# env
+## for executing vscode with root privilege 
+## value for `--user-data-dir` option. see alias **_vsc** of [../.dotfiles.oh-my-zsh/aliases.zsh]
+mkdir $HOME/.config/vscode 
