91304a724b8c74d2b447e0a02eb35af0ed87e66d
GIL B. Chan <bnbcmindnpass@gmail.com>
2019-04-10 11:50:24 +0900

    install rust package(s)

--- rust/install.sh
+++ rust/install.sh
@@ -1,2 +1,6 @@
-# https://www.rust-lang.org/tools/install
+BASE_DIR=$(dirname "$0")
+
+# [rust installation](https://www.rust-lang.org/tools/install)
 curl https://sh.rustup.rs -sSf | sh
+
+bash $BASE_DIR/pkg.sh
