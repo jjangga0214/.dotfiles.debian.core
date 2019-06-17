6bcfa5281ce40246fe37f383411fe59b5a177ea0
GIL B. Chan <bnbcmindnpass@gmail.com>
2019-06-18 07:12:10 +0900

    add python/pipenv installation

--- python/install.sh
+++ python/install.sh
@@ -2,6 +2,7 @@ BASE_DIR=$(dirname "$0")
 
 apt install -y python-pip
 apt install -y python3-pip
+apt install -y pipenv # only works on Debian Buster+. (note that ubuntu 19.04 is based on Debian Buster)
 
 bash $BASE_DIR/pkg.sh
 bash $BASE_DIR/pyenv.sh
