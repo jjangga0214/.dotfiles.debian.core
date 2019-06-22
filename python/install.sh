e066ed9d9cdbd03ef8d5ac3cd456fe8337513e4e
GIL B. Chan <bnbcmindnpass@gmail.com>
2019-06-22 17:56:59 +0900

    fix pkg overriding by pyenv

--- python/install.sh
+++ python/install.sh
@@ -4,5 +4,5 @@ apt install -y python-pip
 apt install -y python3-pip
 apt install -y pipenv # only works on Debian Buster+. (note that ubuntu 19.04 is based on Debian Buster)
 
-bash $BASE_DIR/pkg.sh
 bash $BASE_DIR/pyenv.sh
+bash $BASE_DIR/pkg.sh
